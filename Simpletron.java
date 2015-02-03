24d2237b4d759cc62360df6f0b755ad097db5675
MikeJudge <thejudgement223@gmail.com>
2015-02-02 21:21:54 -0500

    Simpletron is transitioning to using Hex values to represent instructions. Needs bug fixing

--- Simpletron.java
+++ Simpletron.java
@@ -1,105 +1,111 @@
 /**************************************************************************
 * This class runs machine level programs. The machine level programs      *
-* are written as a list of five digit numbers with an operation code     *
+* are written as a list of five digit numbers with an operation code      *
 * corresponding to the first two digits of a five digit decimal number,   * 
-* and the last three digits corresponding to an operand.                    *
+* and the last three digits corresponding to an operand.                  *
 **************************************************************************/
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Simpletron {
-	private static final int MEMORY_SIZE    = 1000;
-	private static final int MAX_WORD_SIZE  = 99999;
-	private static final int MIN_WORD_SIZE  = -99999;
-	private static final int MAX_HEX_LENGTH = Integer.toHexString(MAX_WORD_SIZE).length();
+	private static final int MEMORY_SIZE = 1000;
+	private static final Hex MAX_WORD    = new Hex("+FFFFF");
+	private static final Hex MIN_WORD    = new Hex("-FFFFF");
 
 	//operation code constants
-	private static final int READ       = 10;
-	private static final int WRITE      = 11;
-	private static final int NEWLINE    = 12;
+	private static final Hex READ       = new Hex(10);
+	private static final Hex WRITE      = new Hex(11);
+	private static final Hex NEWLINE    = new Hex(12);
 
-	private static final int LOAD  = 20;
-	private static final int STORE = 21;
+	private static final Hex LOAD       = new Hex(20);
+	private static final Hex STORE      = new Hex(21);
 
-	private static final int ADD       = 30;
-	private static final int SUBTRACT  = 31;
-	private static final int DIVIDE    = 32;
-	private static final int MULTIPLY  = 33;
-	private static final int REMAINDER = 34;
-	private static final int POWER     = 35;
+	private static final Hex ADD        = new Hex(30);
+	private static final Hex SUBTRACT   = new Hex(31);
+	private static final Hex DIVIDE     = new Hex(32);
+	private static final Hex MULTIPLY   = new Hex(33);
+	private static final Hex REMAINDER  = new Hex(34);
+	private static final Hex POWER      = new Hex(35);
 
-	private static final int BRANCH     = 40;
-	private static final int BRANCHNEG  = 41;
-	private static final int BRANCHZERO = 42;
-	private static final int HALT       = 43;
+	private static final Hex BRANCH     = new Hex(40);
+	private static final Hex BRANCHNEG  = new Hex(41);
+	private static final Hex BRANCHZERO = new Hex(42);
+	private static final Hex HALT       = new Hex(43);
 
 
-	private String[] memory;		 //program is stored here
-	private int accumulator;
-	private int instructionCounter;  //location in memory whose instruction is being performed now
+	private Hex[] memory;		 //program is stored here
+	private Hex accumulator;
+	private Hex instructionCounter;  //location in memory whose instruction is being performed now
 
-	private int operationCode;       //operation being currently performed, 1st two numbers of instructionRegister
-	private int operand;		     //memory location where operation is being operated on last three of instructionRegister
-	private int instructionRegister; //full instruction word
+	private Hex operationCode;        //operation being currently performed, 1st two digits of instructionRegister
+	private Hex operand;		         //memory location where operation is being operated on last three of instructionRegister
+	private Hex instructionRegister;  //full instruction word
 
 
 	public Simpletron() {
-		memory = new String[MEMORY_SIZE];
-		Arrays.fill(memory, intToHex(0));
-		accumulator 	    = 0;
-		instructionCounter  = 0;
-		operationCode       = 0;
-		operand             = 0;
-		instructionRegister = 0;
+		memory = new Hex[MEMORY_SIZE];
+		for (int i = 0; i < memory.length; i++)
+			memory[i] = new Hex();
+		accumulator 	    = new Hex();
+		instructionCounter  = new Hex();
+		operationCode       = new Hex();
+		operand             = new Hex();
+		instructionRegister = new Hex();
 	}
 
 	//pre:  index, and word are in range
 	//post: word is stored in memory
-	public void storeWord(int index, int word) {
-		if (word > MAX_WORD_SIZE || word < MIN_WORD_SIZE) {
+	public void storeWord(Hex index, Hex word) {
+		int i = index.toInt();
+
+		if (word.compareTo(MAX_WORD) > 0 || word.compareTo(MIN_WORD) < 0) {
 			fatalError("*** overflow occured ***");
 		}
-		if (index > (MEMORY_SIZE-1) || index < 0) {
+		if (i > (MEMORY_SIZE-1) || i < 0) {
 			fatalError("*** index out of bounds ***");
 		}
 
-		memory[index] = intToHex(word);
+		memory[i] = word;
 	}
 
-	//post: string with hex representation of number
-	private String intToHex(int num) {
-		String s = Integer.toHexString(num).toUpperCase();
-		while (s.length() < MAX_HEX_LENGTH)
-			s = "0" + s;
-		if (num >= 0)
-			s = "+" + s;
-		else
-			s = "-" + s;
-		return s;
+	public void storeWord(int index, int word) {
+		String part1, part2;
+		part1 = new Hex(word / 1000).getString(3);
+		part2 = new Hex(word % 1000).getString(4);
+
+		storeWord(new Hex(index), new Hex(part1 + part2.substring(1)));
 	}
 
-	private int hexToInt(String hexString) {
-		int num = 0;
-		int n = 0;
-		char c;
-		for (int i = hexString.length() - 1; i > 0; i--) {
-			c = hexString.charAt(i);
-			if (c >= '0' && c <= '9')
-				num += (int)Math.pow(16, n) * (c - '0');
-			else
-				num += (int)Math.pow(16, n) * (c - 'A' + 10);
-			n++;
+	private Hex getWord(Hex index) {
+		int i = index.toInt();
+		if (i > (MEMORY_SIZE - 1) || i < 0)
+			fatalError("*** index out of bounds ***");
+
+		Hex word = memory[i];
+
+		if (word.compareTo(MAX_WORD) > 0 || word.compareTo(MIN_WORD) < 0) {
+			fatalError("*** overflow occured ***");
 		}
-		if (hexString.charAt(0) == '-')
-			num *= -1;
-
-		return num;
-
+		return word;
 	}
 
-	//returns false if the accumulator has overflowed the max or min WORD_SIZE
+	//returns false if the accumulator has exceeded the max or min WORD_SIZE
 	private boolean isAccumulatorValid() {
-		if (accumulator > MAX_WORD_SIZE || accumulator < MIN_WORD_SIZE)
+		if (accumulator.compareTo(MAX_WORD) > 0 || accumulator.compareTo(MIN_WORD) < 0)
+			return false;
+		return true;
+	}
+
+	private boolean isOperandValid() {
+		int i = operand.toInt();
+		if (i > MEMORY_SIZE-1 || i < 0)
+			return false;
+		return true;
+	}
+
+	private boolean instructionCounterValid() {
+		int i = instructionCounter.toInt();
+		if (i > MEMORY_SIZE-1 || i < 0)
 			return false;
 		return true;
 	}
@@ -108,78 +114,101 @@ public class Simpletron {
 		Scanner input = new Scanner(System.in);
 		while (true) {
 			//case when branch jumps the program out of bounds
-			if (instructionCounter >= MEMORY_SIZE || instructionCounter < 0)
+			if (!instructionCounterValid())
 				fatalError("*** program execution failed ***");
 
-			instructionRegister = hexToInt(memory[instructionCounter]);
-			operationCode = instructionRegister / 1000;
-			operand = instructionRegister % 1000;
+			instructionRegister.setValue(getWord(instructionCounter));
+			operationCode.setValue(Hex.divide(instructionRegister, new Hex("+1000")));
+			operand.setValue(Hex.mod(instructionRegister, new Hex("+1000")));
 
 			if (!isAccumulatorValid())
 				fatalError("*** Overflow occured ***");
 
-			if (operand >= MEMORY_SIZE || operand < 0)
+			if (!isOperandValid())
 				fatalError("*** operand index out of bounds ***");
 
-			switch (operationCode) {
-				//condense code branch, and branchneg are the only ops that don't instructioncounter++
-				case READ:        System.out.print("Enter an integer: ");
-							      storeWord(operand, input.nextInt());
-							      instructionCounter++;
-							      break;
-				case WRITE:       System.out.print(hexToInt(memory[operand]));
-							      instructionCounter++;
-							      break;
-			    case NEWLINE:     System.out.println();
-			                      instructionCounter++;
-			                      break;
-				case LOAD:        accumulator = hexToInt(memory[operand]);
-							      instructionCounter++;
-							      break;
-				case STORE:       storeWord(operand, accumulator);
-							      instructionCounter++;
-							      accumulator = 0;
-							      break;
-				case ADD:         accumulator += hexToInt(memory[operand]);
-								  instructionCounter++;
-								  break;
-				case SUBTRACT:    accumulator -= hexToInt(memory[operand]);
-								  instructionCounter++;
-								  break;
-				case DIVIDE:      if (hexToInt(memory[operand]) == 0) //can't divide by zero
-        					      	 fatalError("*** attempt to divide by zero ***");
-								  accumulator /= hexToInt(memory[operand]);     
-						          instructionCounter++;
-						          break;
-				case MULTIPLY:    accumulator *= hexToInt(memory[operand]);
-								  instructionCounter++;
-								  break;
-				case REMAINDER:   if (hexToInt(memory[operand]) == 0) //can't divide by zero
-									 fatalError("*** attempt to divide by zero ***");
-								  accumulator %= hexToInt(memory[operand]);
-								  instructionCounter++;
-								  break;
-				case POWER:       accumulator = (int)Math.pow(accumulator,hexToInt(memory[operand]));
-								  instructionCounter++;
-								  break;
-				case BRANCH:      instructionCounter = operand;
-							      break;
-				case BRANCHNEG:   if (accumulator < 0) 
-									  instructionCounter = operand;
-							      else
-							      	  instructionCounter++;
-				                  break;
-				case BRANCHZERO:  if (accumulator == 0) 
-									  instructionCounter = operand;
-								  else
-								  	  instructionCounter++;
-								  break;
-				case HALT:        System.out.println("*** Simpletron execution terminated ***");
-							      return;
-				//invalid operation code
-				default:		  fatalError("*** Invalid operation code ***");
 
-			}
+
+			if (operationCode.equals(READ)) 
+			{
+				System.out.print("Enter an integer: ");
+				storeWord(operand, new Hex(input.nextLong()));
+			} 
+			else if (operationCode.equals(WRITE)) 
+			{
+				System.out.print(getWord(operand).toLong());
+			} 
+			else if (operationCode.equals(NEWLINE)) 
+			{
+				System.out.println();
+			} 
+			else if (operationCode.equals(LOAD)) 
+			{
+				accumulator = getWord(operand);
+			} 
+			else if (operationCode.equals(STORE)) 
+			{
+				storeWord(operand, accumulator);
+				accumulator.setValue(0);
+			} 
+			else if (operationCode.equals(ADD)) 
+			{
+				accumulator.setValue(Hex.add(accumulator, getWord(operand)));
+			} 
+			else if (operationCode.equals(SUBTRACT)) 
+			{
+				accumulator.setValue(Hex.subtract(accumulator, getWord(operand)));
+			} 
+			else if (operationCode.equals(DIVIDE)) 
+			{
+				if (getWord(operand).equals(new Hex(0))) //can't divide by zero
+        			fatalError("*** attempt to divide by zero ***");
+				accumulator.setValue(Hex.divide(accumulator, getWord(operand))); 
+			} 
+			else if (operationCode.equals(MULTIPLY)) 
+			{
+				accumulator.setValue(Hex.multiply(accumulator, getWord(operand))); 
+			} 
+			else if (operationCode.equals(REMAINDER)) 
+			{
+				if (getWord(operand).equals(new Hex(0))) //can't divide by zero
+					fatalError("*** attempt to divide by zero ***");
+				accumulator.setValue(Hex.mod(accumulator, getWord(operand))); 
+			} 
+			else if (operationCode.equals(POWER)) 
+			{
+				accumulator.setValue(Hex.exponent(accumulator, getWord(operand).toInt()));
+			} /////////////modify this to work with longs
+			else if (operationCode.equals(BRANCH)) 
+			{
+			  	instructionCounter.setValue(operand);
+		    } 
+		    else if (operationCode.equals(BRANCHNEG)) 
+		    {
+		    	if (accumulator.compareTo(new Hex(0)) < 0) 
+					instructionCounter.setValue(operand);
+				else
+					instructionCounter.setValue(Hex.add(instructionCounter, new Hex(1)));
+		    } 
+		    else if (operationCode.equals(BRANCHZERO)) 
+		    {
+		    	if (accumulator.equals(new Hex(0))) 
+					instructionCounter.setValue(operand);
+				else
+					instructionCounter.setValue(Hex.add(instructionCounter, new Hex(1)));
+		    } 
+		    else if (operationCode.equals(HALT)) 
+		    {
+		    	System.out.println("*** Simpletron execution terminated ***");
+		    	return;
+		    } 
+		    else 
+		    {
+		    	fatalError("*** Invalid operation code ***");
+		    }
+
+		    if (operationCode.compareTo(BRANCH) < 0)
+		    	instructionCounter.setValue(Hex.add(instructionCounter, new Hex(1)));
 
 		}
 	}
@@ -188,11 +217,11 @@ public class Simpletron {
 	//post: all of the variables are printed to the screen
 	public void dumpMemory() {
 		System.out.println("REGISTERS:");
-		System.out.println("accumulator" + "          " + formatWord(accumulator));
-		System.out.println("instructionCounter" + "   " + "   " + formatCode(instructionCounter, 3));
-		System.out.println("instructionRegister" + "  " + formatWord(instructionRegister));
-		System.out.println("operationCode" + "        " + "    " + formatCode(operationCode, 2));
-		System.out.println("operand" + "              " + "   " + formatCode(operand, 3));
+		System.out.println("accumulator" + "          " + accumulator.getString(6));
+		System.out.println("instructionCounter" + "   " + "  " + instructionCounter.getString(4));
+		System.out.println("instructionRegister" + "  " + instructionRegister.getString(6));
+		System.out.println("operationCode" + "        " + "   " + operationCode.getString(3));
+		System.out.println("operand" + "              " + "  " +  operand.getString(4));
 		System.out.println("\n" + "MEMORY:");
 		System.out.print("   ");
 
@@ -210,12 +239,12 @@ public class Simpletron {
 			else
 				System.out.print(i + "0");
 			for (int n = 0; n < DIMEN; n++) {
-				System.out.print(" " + memory[i*DIMEN + n]);
+				System.out.print(" " + memory[i*DIMEN + n].getString(6));
 			}
 			System.out.println();
 		}
 	}
-
+/*
 	//post: returns word in +vwxyz or -vwxyz format
 	private String formatWord(int word) {
 		String s = Math.abs(word) + "";
@@ -236,7 +265,7 @@ public class Simpletron {
 			s = "0" + s;
 		return s;
 	}
-
+*/
 	private void fatalError(String errorMessage) {
 		System.out.println(errorMessage);
 		System.out.println("*** Simpletron execution abnormally terminated ***");
